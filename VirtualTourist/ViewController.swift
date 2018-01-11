21b441104930b423690659c0d10e06a046cf15e5
Cong Doan <cong.doan@gmail.com>
2018-01-11 08:15:22 +0700

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Cong Doan on 1/11/18.
+//  Copyright © 2018 Cong Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
